9660646bc1281944d5c1d73bae5174c5c721eea4
Daniel Xu <xudh47@gmail.com>
2022-09-04 16:41:30 -0700

    Added pseudo code

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateHTML = require("./generateHTML");
+const generateCSS = require("./generateCSS");
+
+// Create a function to write HTML
+function writeToFile() {}
+
+// Create a function to initialize app
+function init() {
+  console.log("test");
+}
+
+init();
